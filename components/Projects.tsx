61f0ba4f5b40f26be03338d4f0282d66d56e0696
Julio Miguel Gagliardi <juliog_14@hotmail.com>
2022-11-10 14:36:10 -0300

    Added a key to each child of a div

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,7 +18,8 @@ const Projects = (props: Props) => {
 			>
 				{projects.map((project, i) => (
 					<div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5
-          items-center justify-center p-20 md:p-44 h-screen">
+          items-center justify-center p-20 md:p-44 h-screen"
+          key={i}>
 						<img
 							src="https://bregornoriginal.github.io/Julio-Gagliardi-Portfolio/images/ezCar/ezCar.png"
 							alt=""
