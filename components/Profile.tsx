b5e8a10981fb634b41ce6abf29c8396b5dac1cb5
Julio Miguel Gagliardi <juliog_14@hotmail.com>
2022-11-10 01:45:15 -0300

    Added background circles, position header and add a photo

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
-import { Cursor, useTypewriter } from "react-simple-typewriter";
-import BackgroundCircles from "./BackgroundCircles";
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
 
 type Props = {};
 
-export default function Profile({ }: Props) {
-  const [text, count] = useTypewriter({
-    words: [
-      "Hi, My name is Julio Gagliardi",
-      "A gamer and PC's enthusiast",
-      "{ Love write code! }",
-    ],
-    loop: true,
-    delaySpeed: 2000,
-  });
+export default function Profile({}: Props) {
+	const [text, count] = useTypewriter({
+		words: [
+			'Hi, My name is Julio Gagliardi',
+			"A gamer and PC's enthusiast",
+			'{ Love write code! }',
+		],
+		loop: true,
+		delaySpeed: 2000,
+	});
 
-  return (
-  <div className="h-screen flex flex-col space-y-8 items-center justify-center
-  text-center overflow-hidden">
-    <BackgroundCircles />
-    <h1>
-      <span>{text}</span>
-      <Cursor cursorColor="orange" />
-    </h1>
-  </div>
-  );
+	return (
+		<div
+			className="h-screen flex flex-col space-y-8 items-center justify-center
+  text-center overflow-hidden"
+		>
+			<BackgroundCircles />
+			<picture>
+				<img
+					className="relative rounded-full h-32 w-32 mx-auto object-cover"
+					src="https://i.ibb.co/SXQvg99/Foto-JPG.jpg"
+					alt=""
+					width={500}
+					height={500}
+				/>
+			</picture>
+			<div>
+				<h1>
+					<span>{text}</span>
+					<Cursor cursorColor="orange" />
+				</h1>
+			</div>
+		</div>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,22 @@ import Header from '../components/Header';
 import Profile from '../components/Profile';
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-black text-white h-screen snap-y snap-mandatory
-    overflow-scroll z-0">
-      <Head>
-        <title>Julio Gagliardi&apos;s Next Portfolio</title>
-      </Head>
+	return (
+		<div
+			className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    overflow-scroll z-0"
+		>
+			<Head>
+				<title>Julio Gagliardi&apos;s Next Portfolio</title>
+			</Head>
 
-      <Header />
+			<Header />
 
-      <section id="profile">
-        <Profile />
-      </section>
-    </div>
-  );
-}
+			<section id="profile" className="snap-center">
+				<Profile />
+			</section>
+		</div>
+	);
+};
 
 export default Home;
