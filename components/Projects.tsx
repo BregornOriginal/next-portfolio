de0990b4bc568a2a5d0008bbd6322e1c1669ccab
Julio Miguel Gagliardi <juliog_14@hotmail.com>
2022-11-10 14:33:22 -0300

    Map projects to render differents projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,6 +12,26 @@ const Projects = (props: Props) => {
 		>
 			<h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2x1">Projects</h3>
 
+			<div
+				className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory
+      z-20"
+			>
+				{projects.map((project, i) => (
+					<div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5
+          items-center justify-center p-20 md:p-44 h-screen">
+						<img
+							src="https://bregornoriginal.github.io/Julio-Gagliardi-Portfolio/images/ezCar/ezCar.png"
+							alt=""
+						/>
+            <div>
+              <h4>
+                Project {i + 1} of {projects.length}:
+              </h4>
+            </div>
+					</div>
+				))}
+			</div>
+
 			<div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
 		</div>
 	);
