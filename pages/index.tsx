cba3d47ff701be5047ee5d35561aa9a3891c9fa5
Julio Miguel Gagliardi <juliog_14@hotmail.com>
2022-11-09 23:06:14 -0300

    Setup Next.js with Tailwind, EsLint

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Julio Gagliardi&apos;s Next Portfolio</title>
+        <meta name="description" content="Full Stack Developer portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <div className="">
+      <p className="text-2x1 text-green-600 ">
+      Portfolio
+      </p> </div>
+    </div>
+  )
+}
