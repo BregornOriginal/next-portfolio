0de13d0e595a9fae79f330bf5b3b1edf0118de10
Julio Miguel Gagliardi <juliog_14@hotmail.com>
2022-11-21 19:47:10 -0300

    Fix styles on skills logo

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -18,13 +18,13 @@ const Skill = ({ skill, directionLeft }: Props) => {
 				}}
 				transition={{ duration: 1 }}
 				whileInView={{ opacity: 1, x: 0 }}
-				className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28
-				md:h-28 xl:w-32 xl:h-32 filter group:hover:grayscale transition duration-300 ease-in-out"
+				className="rounded-lg border border-gray-500 object-cover w-20 h-20 md:w-24
+				md:h-24 xl:w-26 xl:h-26 filter group:hover:grayscale transition duration-300 ease-in-out"
 				src={urlFor(skill?.image).url()}
 			/>
 			<div
 				className="absolute opacity-0 group-hover:opacity-80 transition duration-300
-			ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32
+			ease-in-out group-hover:bg-white h-20 w-20 md:w-24 md:h-24 xl:w-26 xl:h-26
 			rounded-full z-0"
 			>
 				<div className="flex items-center justify-center h-full">
