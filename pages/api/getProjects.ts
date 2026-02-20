af6fd9af69c77156851f1b91c0f395499df45a29
Gagliardi Julio Miguel <juliog_14@hotmail.com>
2026-02-20 12:41:05 -0300

    set project order

--- pages/api/getProjects.ts
+++ pages/api/getProjects.ts
@@ -6,7 +6,7 @@ import { sanityClient } from '../../sanity';
 import { Project } from '../../typings';
 
 const query = groq`
-  *[_type == "project"] {
+  *[_type == "project"] | order(_createdAt desc) {
 		...,
 		technologies[]->
 	}
