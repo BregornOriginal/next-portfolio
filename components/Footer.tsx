679859d2c9f6d0c9d42eee5d018886e3a6cc558b
Gagliardi Julio Miguel <juliog_14@hotmail.com>
2026-02-27 13:28:04 -0300

    fix unnecesary class

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = (props: Props) => {
 		<footer className="sticky bottom-5 w-full cursor-pointer">
 			<div className="flex flex-end items-center pr-6 justify-end">
 				<img
-					className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer rounded-md object-cover"
+					className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer object-cover"
 					src="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2022/04/19/16503736092836.jpg"
 					alt="An image to homepage"
 					width={40}
